3f289a32ec572c1e2a20f0d5421f5021e9313295
Amund Eggen Svandal <aesvandal@gmail.com>
2021-11-29 15:57:19 +0100

    Add lobby leave and join
    Add more debug logging

--- sidelay.py
+++ sidelay.py
@@ -408,7 +408,11 @@ def remove_ranks(playerstring: str) -> str:
 
 def process_chat_message(message: str, state: OverlayState) -> bool:
     """Look for potential state changes in a chat message and perform them"""
+    # Lobby changes
     WHO_PREFIX = "ONLINE: "
+
+    logger.debug(f"Chat message: '{message}'")
+
     if message.startswith(WHO_PREFIX):
         # Info [CHAT] ONLINE: <username1>, <username2>, ..., <usernameN>
         # Results from /who -> override lobby_players
@@ -418,6 +422,77 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
 
         return True
 
+    if " has joined (" in message:
+        # Info [CHAT] <username> has joined (<x>/<N>)!
+        # Someone joined the lobby -> Add them to the lobby
+        logger.debug("Processing potential lobby join message")
+
+        words = message.split(" ")
+        if len(words) < 4:
+            # The message can not be <username> has joined (<x>/<N>)!
+            logger.debug("Message is too short!")
+            return False
+
+        for word, target in zip(words[1:3], ("has", "joined")):
+            if word != target:
+                logger.debug("Message does not match target! {word=} != {target=}")
+                return False
+
+        lobby_fill_string = words[3]
+        if not re.fullmatch(r"\(\d+\/\d+\)\!", lobby_fill_string):
+            logger.debug("Fill string '{lobby_fill_string}' does not match '(x/N)!'")
+            return False
+
+        prefix, suffix = lobby_fill_string.split("/")
+        player_count = int(prefix.removeprefix("("))
+        player_cap = int(suffix.removesuffix(")!"))
+
+        if player_cap < 8:
+            logger.debug("Gamemode has too few players to be bedwars. Skipping.")
+            return False
+
+        if player_count != len(state.lobby_players) + 1:
+            # We are out of sync with the lobby.
+            # This happens when you first join a lobby, as the previous lobby is
+            # never cleared. It could also be due to a bug
+            logger.debug(
+                "Player count out of sync. Clearing the lobby. Please use /who"
+            )
+            state.set_lobby(state.party_members)
+
+        username = words[0]
+
+        state.add_to_lobby(username)
+
+        logger.info(f"{username} joined your lobby")
+
+        return True
+
+    if " has quit!" in message:
+        # Info [CHAT] <username> has quit!
+        # Someone left the lobby -> Remove them from the lobby
+        logger.debug("Processing potential lobby leave message")
+
+        words = message.split(" ")
+        if len(words) < 3:
+            # The message can not be <username> has quit!
+            logger.debug("Message is too short!")
+            return False
+
+        for word, target in zip(words[1:3], ("has", "quit!")):
+            if word != target:
+                logger.debug("Message does not match target! {word=} != {target=}")
+                return False
+
+        username = words[0]
+
+        state.remove_from_lobby(username)
+
+        logger.info(f"{username} left your lobby")
+
+        return True
+
+    # Party changes
     if message.startswith("You left the party."):
         # Info [CHAT] You left the party.
         # Leaving the party -> remove all but yourself from the party
@@ -431,6 +506,8 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
     if message.startswith(PARTY_YOU_JOIN_PREFIX):
         # Info [CHAT] You have joined [MVP++] <username>'s party!
         # You joined a player's party -> add them to your party
+        logger.debug("Processing potential party you join message")
+
         suffix = message.removeprefix(PARTY_YOU_JOIN_PREFIX)
 
         try:
@@ -452,15 +529,19 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
     if " joined the party" in message:
         # Info [CHAT] [VIP+] <username> joined the party.
         # Someone joined your party -> add them to your party
+        logger.debug("Processing potential party they join message")
+
         suffix = remove_ranks(message)
 
         words = suffix.split(" ")
         if len(words) < 4:
             # The message can not be <username> joined the party
+            logger.debug("Message is too short!")
             return False
 
-        for word, target in zip(words[1:4], ("joined", "the", "party")):
-            if not word.startswith(target):
+        for word, target in zip(words[1:4], ("joined", "the", "party.")):
+            if word != target:
+                logger.debug("Message does not match target! {word=} != {target=}")
                 return False
 
         username = words[0]
@@ -474,15 +555,19 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
     if " left the party" in message:
         # Info [CHAT] [VIP+] <username> left the party.
         # Someone left your party -> remove them from your party
+        logger.debug("Processing potential party they leave message")
+
         suffix = remove_ranks(message)
 
         words = suffix.split(" ")
         if len(words) < 4:
             # The message can not be <username> left the party
+            logger.debug("Message is too short!")
             return False
 
         for word, target in zip(words[1:4], ("left", "the", "party")):
             if not word.startswith(target):
+                logger.debug("Message does not match target! {word=} != {target=}")
                 return False
 
         username = words[0]
@@ -509,6 +594,10 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
         # Info [CHAT] Party Members (<n>)
         # This is a response from /pl (/party list)
         # In the following lines we will get all the party members -> clear the party
+        logger.debug(
+            "Receiving response from /pl -> clearing party and awaiting further data"
+        )
+
         state.clear_party()
 
         return False  # No need to redraw as we're waiting for further input
@@ -519,7 +608,7 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
     for prefix in (PARTY_LEADER_PREFIX, PARTY_MODERATORS_PREFIX, PARTY_MEMBERS_PREFIX):
         # Info [CHAT] Party <role>: [MVP++] <username> ●
         if message.startswith(prefix):
-            logger.debug(f"Updating party members from {prefix}")
+            logger.debug(f"Updating party members from {prefix.split(' ')[2]}")
 
             suffix = message.removeprefix(prefix)
             dirty_string = remove_ranks(suffix)
@@ -542,6 +631,7 @@ def process_chat_message(message: str, state: OverlayState) -> bool:
 
 
 def process_loglines(loglines: Iterable[str]) -> None:
+    """Process the state changes for each logline and redraw the screen if neccessary"""
     state = OverlayState(lobby_players=set(), party_members=set(), own_username=None)
 
     for line in loglines:
@@ -572,12 +662,14 @@ def process_loglines(loglines: Iterable[str]) -> None:
 
 
 def watch_from_logfile(logpath: str) -> None:
+    """Use the overlay on an active logfile"""
     with open(logpath, "r") as logfile:
         loglines = follow(logfile)
         process_loglines(loglines)
 
 
 def test() -> None:
+    """Test the implementation on a static logfile"""
     global TESTING
 
     TESTING = True
