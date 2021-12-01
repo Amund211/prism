a40b843b16bfb1a0f924c8305a783011b198d5b5
Amund Eggen Svandal <aesvandal@gmail.com>
2021-12-01 01:33:12 +0100

    Start adding tests

--- src/hystatutils/calc.py
+++ src/hystatutils/calc.py
@@ -51,24 +51,3 @@ def bedwars_level_from_exp(exp: int) -> float:
         next_level_cost = LEVEL_COST
 
     return levels + exp / next_level_cost
-
-
-if __name__ == "__main__":
-    bw_star_data = [
-        (500, 1),
-        (89025, 20 + 2025 / 5000),
-        (122986, 27),
-        (954638, 196),
-        (969078, 199),
-        (975611, 202),
-        (977587, 203),
-        (2344717, 481 + 4717 / 5000),
-    ]
-    for exp, true_star in bw_star_data:
-        calculated_star = bedwars_level_from_exp(exp)
-
-        # Compare int with int, and float with float
-        if isinstance(true_star, int):
-            calculated_star = int(calculated_star)
-
-        assert true_star == calculated_star, f"{exp=}: {calculated_star=}, {true_star=}"
---
+++ tests/hystatutils/test_calc.py
@@ -0,0 +1,22 @@
+import pytest
+from hystatutils.calc import bedwars_level_from_exp
+
+
+@pytest.mark.parametrize("exp, true_star", [
+    (500, 1),
+    (89025, 20 + 2025 / 5000),
+    (122986, 27),
+    (954638, 196),
+    (969078, 199),
+    (975611, 202),
+    (977587, 203),
+    (2344717, 481 + 4717 / 5000),
+])
+def test_meme_test(exp, true_star):
+    calculated_star = bedwars_level_from_exp(exp)
+
+    # Compare int with int, and float with float
+    if isinstance(true_star, int):
+        calculated_star = int(calculated_star)
+
+    assert true_star == calculated_star
