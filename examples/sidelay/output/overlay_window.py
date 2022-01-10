59e6b5161267065ccdf2549096e2330ffbe940cf
Amund Eggen Svandal <aesvandal@gmail.com>
2022-01-11 00:09:16 +0100

    Revert "Use iconify instead of withdraw"
    This reverts commit 51cf57268f6a495236441a9e7ef0a3c5843774b3.
    Can't iconify when overrideredirect(True)

--- examples/sidelay/output/overlay_window.py
+++ examples/sidelay/output/overlay_window.py
@@ -226,7 +226,7 @@ class OverlayWindow(Generic[ColumnKey]):
 
     def hide_window(self) -> None:
         """Hide the window"""
-        self.root.iconify()
+        self.root.withdraw()
         self.shown = False
 
     def append_row(self) -> None:
