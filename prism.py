371c4e364b5f1ca12c4c3db7985d391ab219fc29
Amund Eggen Svandal <aesvandal@gmail.com>
2022-01-20 03:25:19 +0100

    Log exceptions caught in main

--- prism.py
+++ prism.py
@@ -1,6 +1,12 @@
+import logging
 from pathlib import Path
 
 from examples.sidelay.__main__ import main
 
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    main(Path(__file__).parent.resolve() / "data" / "nick_database.json")
+    try:
+        main(Path(__file__).parent.resolve() / "data" / "nick_database.json")
+    except Exception:
+        logger.exception("Exception caught in main")
