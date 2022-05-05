426aa3455951db9641e754bf7b242588235bd982
Amund Eggen Svandal <aesvandal@gmail.com>
2022-05-05 12:07:51 +0200

    Update threshold for fkdr

--- examples/overlay/output/utils.py
+++ examples/overlay/output/utils.py
@@ -12,7 +12,7 @@ COLUMN_NAMES: dict[PropertyName, str] = {
 
 STAT_LEVELS: dict[PropertyName, Optional[Sequence[Union[int, float]]]] = {
     "stars": (100, 300, 500, 800),
-    "fkdr": (0.5, 2, 4, 8),
+    "fkdr": (1, 2, 4, 8),
     "wlr": (0.3, 1, 2, 4),
     "winstreak": (5, 15, 30, 50),
     "username": None,
