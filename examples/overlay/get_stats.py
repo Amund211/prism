cf86f8891c9e94716cdfef6fe9fcd2565b97a6fa
Amund Eggen Svandal <aesvandal@gmail.com>
2022-06-10 11:55:40 +0200

    Refactor fetch_bedwars_stats to be pure

--- examples/overlay/get_stats.py
+++ examples/overlay/get_stats.py
@@ -1,14 +1,9 @@
+import functools
 import logging
 from dataclasses import replace
-from typing import Callable
+from typing import Any, Callable
 
-from examples.overlay.player import (
-    KnownPlayer,
-    NickedPlayer,
-    PendingPlayer,
-    Player,
-    Stats,
-)
+from examples.overlay.player import KnownPlayer, NickedPlayer, PendingPlayer, Stats
 from examples.overlay.player_cache import get_cached_player, set_cached_player
 from prism.calc import bedwars_level_from_exp
 from prism.minecraft import MojangAPIError, get_uuid
@@ -24,19 +19,33 @@ from prism.utils import div
 logger = logging.getLogger(__name__)
 
 
+def get_uuid_from_mojang(username: str) -> str | None:  # pragma: nocover
+    """Lookup uuid from Mojang"""
+    try:
+        return get_uuid(username)
+    except MojangAPIError as e:
+        logger.debug(f"Failed getting uuid for username {username} {e=}")
+        return None
+
+
+def get_player_data_from_hypixel(
+    uuid: str, key_holder: HypixelAPIKeyHolder
+) -> dict[str, Any] | None:  # pragma: nocover
+    try:
+        return get_player_data(uuid, key_holder)
+    except HypixelAPIError as e:
+        logger.debug(f"Failed getting stats for {uuid=} {e=}")
+        return None
+
+
 def fetch_bedwars_stats(
     username: str,
-    key_holder: HypixelAPIKeyHolder,
+    get_uuid: Callable[[str], str | None],
+    get_player_data: Callable[[str], dict[str, Any] | None],
     denick: Callable[[str], str | None] = lambda nick: None,
-) -> KnownPlayer | NickedPlayer:  # pragma: nocover
+) -> KnownPlayer | NickedPlayer:
     """Fetches the bedwars stats for the given player"""
-    # Lookup uuid from Mojang
-    try:
-        uuid = get_uuid(username)
-    except MojangAPIError as e:
-        # No match from Mojang -> assume the username is a nickname
-        logger.debug(f"Failed getting uuid for username {username} {e=}")
-        uuid = None
+    uuid = get_uuid(username)
 
     # Look up in nick database if we got no match from Mojang
     nick: str | None = None
@@ -49,82 +58,72 @@ def fetch_bedwars_stats(
             denicked = True
             logger.debug(f"De-nicked {username} as {uuid}")
 
-    player: Player
     if uuid is None:
-        player = NickedPlayer(nick=username)
+        return NickedPlayer(nick=username)
+
+    playerdata = get_player_data(uuid)
+    if playerdata is None:
+        logger.debug(
+            f"Failed initially getting stats for {username} ({uuid}) {denicked=} "
+        )
+
+    if not denicked and playerdata is None:
+        denick_result = denick(username)
+        if denick_result is not None:
+            # The username may be an existing minecraft account that has not
+            # logged on to Hypixel. Then we would get a hit from Mojang, but
+            # no hit from Hypixel and the username is still a nickname.
+            uuid = denick_result
+            nick = username
+            logger.debug(f"De-nicked {username} as {uuid} after hit from Mojang")
+            playerdata = get_player_data(uuid)
+            if playerdata is None:
+                logger.debug(f"Failed getting stats for nicked {nick} ({uuid})")
+
+    if playerdata is None:
+        logger.debug("Got no playerdata - assuming player is nicked")
+        return NickedPlayer(nick=username)
+
+    if nick is not None:
+        # Successfully de-nicked - update actual username
+        username = playerdata["displayname"]
+        logger.debug(f"De-nicked {nick} as {username}")
+
+    try:
+        bw_stats = get_gamemode_stats(playerdata, gamemode="Bedwars")
+    except MissingStatsError:
+        return KnownPlayer(
+            username=username,
+            uuid=uuid,
+            stars=0,
+            stats=Stats(
+                fkdr=0,
+                wlr=0,
+                winstreak=0,
+                winstreak_accurate=True,
+            ),
+        )
     else:
-        try:
-            playerdata = get_player_data(uuid, key_holder)
-        except HypixelAPIError as e:
-            logger.debug(
-                f"Failed initially getting stats for {username} ({uuid}) {denicked=} "
-                f"{e=}"
-            )
-            playerdata = None
-
-        if not denicked and playerdata is None:
-            denick_result = denick(username)
-            if denick_result is not None:
-                # The username may be an existing minecraft account that has not
-                # logged on to Hypixel. Then we would get a hit from Mojang, but
-                # no hit from Hypixel and the username is still a nickname.
-                uuid = denick_result
-                nick = username
-                logger.debug(f"De-nicked {username} as {uuid} after hit from Mojang")
-                try:
-                    playerdata = get_player_data(uuid, key_holder)
-                except HypixelAPIError as e:
-                    logger.debug(
-                        f"Failed getting stats for nicked {nick} ({uuid}) {e=}"
-                    )
-                    playerdata = None
-
-        if playerdata is None:
-            logger.debug("Got no playerdata - assuming player is nicked")
-            player = NickedPlayer(nick=username)
-        else:
-            if nick is not None:
-                # Successfully de-nicked - update actual username
-                username = playerdata["displayname"]
-                logger.debug(f"Updating de-nicked {username=}")
-
-            try:
-                bw_stats = get_gamemode_stats(playerdata, gamemode="Bedwars")
-            except MissingStatsError:
-                player = KnownPlayer(
-                    username=username,
-                    uuid=uuid,
-                    stars=0,
-                    stats=Stats(
-                        fkdr=0,
-                        wlr=0,
-                        winstreak=0,
-                        winstreak_accurate=True,
-                    ),
-                )
-            else:
-                winstreak = bw_stats.get("winstreak", None)
-                player = KnownPlayer(
-                    username=username,
-                    nick=nick,
-                    uuid=uuid,
-                    stars=bedwars_level_from_exp(bw_stats.get("Experience", 500)),
-                    stats=Stats(
-                        fkdr=div(
-                            bw_stats.get("final_kills_bedwars", 0),
-                            bw_stats.get("final_deaths_bedwars", 0),
-                        ),
-                        wlr=div(
-                            bw_stats.get("wins_bedwars", 0),
-                            bw_stats.get("games_played_bedwars", 0)
-                            - bw_stats.get("wins_bedwars", 0),
-                        ),
-                        winstreak=winstreak,
-                        winstreak_accurate=winstreak is not None,
-                    ),
-                )
-
-    return player
+        winstreak = bw_stats.get("winstreak", None)
+        return KnownPlayer(
+            username=username,
+            nick=nick,
+            uuid=uuid,
+            stars=bedwars_level_from_exp(bw_stats.get("Experience", 500)),
+            stats=Stats(
+                fkdr=div(
+                    bw_stats.get("final_kills_bedwars", 0),
+                    bw_stats.get("final_deaths_bedwars", 0),
+                ),
+                wlr=div(
+                    bw_stats.get("wins_bedwars", 0),
+                    bw_stats.get("games_played_bedwars", 0)
+                    - bw_stats.get("wins_bedwars", 0),
+                ),
+                winstreak=winstreak,
+                winstreak_accurate=winstreak is not None,
+            ),
+        )
 
 
 def get_bedwars_stats(
@@ -143,7 +142,12 @@ def get_bedwars_stats(
     logger.debug(f"Cache miss {username}")
 
     player = fetch_bedwars_stats(
-        username=username, key_holder=key_holder, denick=denick
+        username=username,
+        get_uuid=get_uuid_from_mojang,
+        get_player_data=functools.partial(
+            get_player_data_from_hypixel, key_holder=key_holder
+        ),
+        denick=denick,
     )
 
     # Set the cache
