f34117396fee9869a3ba565738c195277831fa40
Amund Eggen Svandal <aesvandal@gmail.com>
2023-02-27 03:15:32 +0100

    Increase connection pool size to match concurrency

--- src/prism/requests.py
+++ src/prism/requests.py
@@ -1,4 +1,5 @@
 import requests
+import requests.adapters
 
 from prism import USER_AGENT
 
@@ -7,4 +8,12 @@ def make_prism_requests_session() -> requests.Session:
     """Create a requests session object for making http requests"""
     session = requests.Session()
     session.headers.update({"User-Agent": USER_AGENT})
+
+    # Mount adapters with space for 16 connections, as we run with 16 threads by default
+    # Using the default of 10, the 11th-16th active connections at any time will
+    # be discarded after use.
+    for prefix in ("https://", "http://"):
+        adapter = requests.adapters.HTTPAdapter(pool_connections=16, pool_maxsize=16)
+        session.mount(prefix, adapter)
+
     return session
