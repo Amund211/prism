c1fb23ea0c0442ed403e88fbeaa3915a90181f90
Amund Eggen Svandal <aesvandal@gmail.com>
2023-05-27 15:08:28 +0200

    Change username in testing to Testing

--- src/prism/overlay/__main__.py
+++ src/prism/overlay/__main__.py
@@ -228,18 +228,18 @@ def test() -> None:  # pragma: nocover
     else:
         CHAT = "(Client thread) Info [CHAT] "
         loglines = [
-            "(Client thread) Info Setting user: You",
+            "(Client thread) Info Setting user: Testing",
             f"{CHAT}You have joined [MVP++] Teammate's party!",
             f"{CHAT}You'll be partying with: Notch",
             f"{CHAT}Teammate has joined (2/16)!",  # out of sync
-            f"{CHAT}You has joined (3/16)!",
-            f"{CHAT}ONLINE: You, Teammate, Hypixel",  # in sync
+            f"{CHAT}Testing has joined (3/16)!",
+            f"{CHAT}ONLINE: Testing, Teammate, Hypixel",  # in sync
             f"{CHAT}Technoblade has joined (4/16)!",
             f"{CHAT}Manhal_IQ_ has joined (5/16)!",
             f"{CHAT}edater has joined (6/16)!",  # denicked by api
             f"{CHAT}SomeUnknownNick has joined (7/16)!",  # Nicked teammate
             # f"{CHAT}               Bed Wars ",  # game start
-            f"{CHAT}Hypixel was killed by You. FINAL KILL!",
+            f"{CHAT}Hypixel was killed by Testing. FINAL KILL!",
             # f"{CHAT}1st Killer - [MVP+] Player1",  # game end
         ]
 
