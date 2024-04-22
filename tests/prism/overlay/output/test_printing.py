18841904865e68be1796f5820c3c228c8f20bcbc
Amund Eggen Svandal <aesvandal@gmail.com>
2024-04-22 21:06:41 +0200

    chore: Add test for print_stats_table
    Only tests that it runs without crashing

--- tests/prism/overlay/output/test_printing.py
+++ tests/prism/overlay/output/test_printing.py
@@ -1,5 +1,6 @@
-from prism.overlay.output.cells import ColumnName
-from prism.overlay.output.printing import SEP, color, get_sep, title
+from prism.overlay.output.cells import DEFAULT_COLUMN_ORDER, ColumnName
+from prism.overlay.output.printing import SEP, color, get_sep, print_stats_table, title
+from tests.prism.overlay.utils import DEFAULT_RATING_CONFIG_COLLECTION, make_player
 
 END = "\033[0m"
 
@@ -22,3 +23,14 @@ def test_get_sep() -> None:
     assert get_sep("winstreak", column_order) == "\n"
 
     assert get_sep("someweirdstring", column_order) == SEP
+
+
+def test_print_stats_table() -> None:
+    """Assert that it doesn't throw an error"""
+    print_stats_table(
+        [make_player()],
+        set(),
+        DEFAULT_COLUMN_ORDER,
+        DEFAULT_RATING_CONFIG_COLLECTION,
+        out_of_sync=False,
+    )
