ca6cc8559eb1d7942a730e6d952c3d216b510beb
Amund Eggen Svandal <aesvandal@gmail.com>
2024-08-02 19:04:07 +0200

    chore: Adapt pip_system_certs
    Formatting, typing, removed unused hooks, moved wrapt function to the
    bottom as it wasn't working when on top.

--- src/prism/use_system_certs.py
+++ src/prism/use_system_certs.py
@@ -25,42 +25,37 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
 Copied from https://gitlab.com/alelec/pip-system-certs
+
+Adapted for use in Prism
 """
 
-from __future__ import absolute_import
 import warnings
-import wrapt
+from typing import TYPE_CHECKING, Any, cast
 
-## Enable the import to enable type hinting / code navigation below
-# import pip._vendor.requests.adapters
+import wrapt  # type: ignore
 
-
-# pip master has most commands moved into _internal folder
-
-
-@wrapt.when_imported('requests')
-@wrapt.when_imported('pip._vendor.requests')
-# Support for pipenv v2022.8.5
-@wrapt.when_imported('pipenv.patched.pip._vendor.requests')
-# Support for pipenv older than v2022.8.5
-@wrapt.when_imported('pipenv.patched.notpip._vendor.requests')
-def apply_patches(requests):
-    override_ssl_handler(requests.adapters.HTTPAdapter)
+if TYPE_CHECKING:  # pragma: no cover
+    import requests
+    import requests.adapters
+    import urllib3.connection
 
 
-def override_ssl_handler(adapter):
-    # type: (pip._vendor.requests.adapters.HTTPAdapter) -> None
-    
-    def init_poolmanager(wrapped, _instance, args, kwargs):
-        # type: (pip._vendor.requests.adapters.HTTPAdapter.init_poolmanager, None, list, dict) -> None
+def override_ssl_handler(
+    adapter: "requests.adapters.HTTPAdapter",
+) -> None:  # pragma: no cover
+    def init_poolmanager(
+        wrapped: Any, _instance: None, args: list[object], kwargs: dict[str, object]
+    ) -> None:
         import ssl
+
         ssl_context = ssl.create_default_context()
         ssl_context.load_default_certs()
-        kwargs['ssl_context'] = ssl_context
+        kwargs["ssl_context"] = ssl_context
         wrapped(*args, **kwargs)
 
-    def cert_verify(wrapped, _instance, args, kwargs):
-        # type: (pip._vendor.requests.adapters.HTTPAdapter.cert_verify, None, list, dict) -> None
+    def cert_verify(
+        wrapped: Any, _instance: None, args: list[object], kwargs: dict[str, object]
+    ) -> None:
         wrapped(*args, **kwargs)
 
         # By default Python requests uses the ca_certs from the certifi module
@@ -71,11 +66,13 @@ def override_ssl_handler(adapter):
         else:
             conn = args[0]
 
+        conn = cast("urllib3.connectionpool.HTTPSConnectionPool", conn)
+
         # our default ssl_context we created from init_poolmanager() has these attributes set:
         #  - check_hostname == True
         #  - verify_mode == VerifyMode.CERT_REQUIRED
         # this ssl_context gets passed all the way down to the HTTPSConnection object from urllib3
-        # and urllib3 wants to set ssl_context.verify_mode depending on the verify parameter 
+        # and urllib3 wants to set ssl_context.verify_mode depending on the verify parameter
         # (verify is either a bool where False becomes CERT_NONE; or a string pointing to ca bundle)
         # However, the SSLContext class doesnt allow this while check_hostname is True
         # Therefore, we reset check_hostname in case cert_verify() got called with verify == False
@@ -84,7 +81,7 @@ def override_ssl_handler(adapter):
         elif len(args) > 2:
             verify = args[2]
         else:
-            # this is not reachable because there is no default for this parameter 
+            # this is not reachable because there is no default for this parameter
             # but it prepares us in case urllib3 might add a default one day
             verify = True
             warnings.warn(
@@ -106,5 +103,10 @@ def override_ssl_handler(adapter):
 
         conn.ca_certs = None
 
-    wrapt.wrap_function_wrapper(adapter, 'init_poolmanager', init_poolmanager)
-    wrapt.wrap_function_wrapper(adapter, 'cert_verify', cert_verify)
+    wrapt.wrap_function_wrapper(adapter, "init_poolmanager", init_poolmanager)
+    wrapt.wrap_function_wrapper(adapter, "cert_verify", cert_verify)
+
+
+@wrapt.when_imported("requests")  # type: ignore  # pragma: no cover
+def apply_patches(requests: Any) -> None:
+    override_ssl_handler(requests.adapters.HTTPAdapter)
--- tests/prism/overlay/test_imports.py
+++ tests/prism/overlay/test_imports.py
@@ -1,3 +1,4 @@
+# isort: skip_file
 """
 Test that all the main files can be imported
 """
@@ -9,3 +10,13 @@ def test_main() -> None:
 
 def test_testing() -> None:
     import prism.overlay.testing  # noqa: F401
+
+
+def test_use_system_certs() -> None:
+    # Import use_system_certs first to enable patching
+    import prism.use_system_certs  # noqa: F401
+
+    # Import requests as well to make sure the patching works
+    # NOTE: Requests is likely imported already by this point, so the patching
+    # will likely not get tested here
+    import requests  # noqa: F401
