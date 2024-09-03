b6ab35db821bd09e85c889a4405969079fb3f489
Amund Eggen Svandal <aesvandal@gmail.com>
2024-09-03 21:00:33 +0200

    chore: Update loglines used in testing

--- src/prism/overlay/testing.py
+++ src/prism/overlay/testing.py
@@ -32,15 +32,11 @@ def get_test_loglines(options: Options) -> Iterable[str]:  # pragma: nocover
             "(Client thread) Info Setting user: Testing",
             f"{CHAT}You have joined [MVP++] Teammate's party!",
             f"{CHAT}You'll be partying with: Notch",
-            f"{CHAT}Teammate has joined (2/16)!",  # out of sync
-            f"{CHAT}Testing has joined (3/16)!",
-            f"{CHAT}ONLINE: Testing, Teammate, Hypixel",  # in sync
-            f"{CHAT}Technoblade has joined (4/16)!",
-            f"{CHAT}Manhal_IQ_ has joined (5/16)!",
-            f"{CHAT}edater has joined (6/16)!",  # denicked by api
-            f"{CHAT}SomeUnknownNick has joined (7/16)!",  # Nicked teammate
+            f"{CHAT}sdflksjdfh has joined (2/16)!",
+            f"{CHAT}sdflksjddfoih has joined (3/16)!",
+            f"{CHAT}ONLINE: Testing, Teammate, Hypixel, Technoblade, Manhal_IQ_, SomeUnknownNick",  # noqa: E501
             # f"{CHAT}               Bed Wars ",  # game start
-            f"{CHAT}Hypixel was killed by Testing. FINAL KILL!",
+            # f"{CHAT}Hypixel was killed by Testing. FINAL KILL!",
             # f"{CHAT}1st Killer - [MVP+] Player1",  # game end
         ]
 
